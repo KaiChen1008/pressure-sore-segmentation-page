3fdbe2ce41c196f098bba052349a97cbf52b130c
KaiChen1008 <1038790@gmail.com>
2021-02-22 23:05:15 +0800

    change gpu id

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ from .prediction_handler import PreditionHandler
 
 warnings.filterwarnings('ignore')
 os.environ["CUDA_DEVICE_ORDER"]    = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["CUDA_VISIBLE_DEVICES"] = "6"
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = "3" # tensorflow only
 
 
